4405a548c872514ed81403ee5a309b100a6343c4
srijaallam <srija.allam10@gmail.com>
2022-05-17 10:43:02 -0400

    Add files via upload

---
+++ run-container.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm -e "NODE_ENV=fortinet_config" -v $(pwd)/fortinet_config.yaml:/juice-shop/config/fortinet_config.yaml -v $(pwd)/ftnt_favicon.ico:/juice-shop/frontend/dist/frontend/assets/public/ftnt_favicon.ico -v $(pwd)/ftnt_logo.png:/juice-shop/frontend/dist/frontend/assets/public/images/ftnt_logo.png -p 3000:3000 --name juice-shop bkimminich/juice-shop
